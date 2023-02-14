f6dddf117c6b63aa7157855e06d4a634c4308ce1
Maxim Kuznetsov <d1sma@yandex.ru>
2023-02-15 00:10:37 +0300

    Review fixes: use update metric lock

--- internal/metrics/counterMetric.go
+++ internal/metrics/counterMetric.go
@@ -1,16 +1,22 @@
 package metrics
 
-import "github.com/MaxReX92/go-yandex-aka-prometheus/internal/parser"
+import (
+	"sync"
+
+	"github.com/MaxReX92/go-yandex-aka-prometheus/internal/parser"
+)
 
 type counterMetric struct {
 	name  string
 	value int64
+	lock  sync.RWMutex
 }
 
 func NewCounterMetric(name string) Metric {
 	return &counterMetric{
 		name:  name,
 		value: 0,
+		lock:  sync.RWMutex{},
 	}
 }
 
@@ -23,13 +29,21 @@ func (m *counterMetric) GetName() string {
 }
 
 func (m *counterMetric) GetStringValue() string {
+	m.lock.RLock()
+	defer m.lock.RUnlock()
 	return parser.IntToString(m.value)
 }
 
 func (m *counterMetric) SetValue(value float64) {
-	m.value = m.value + int64(value)
+	m.setValue(m.value + int64(value))
 }
 
 func (m *counterMetric) Flush() {
-	m.value = 0
+	m.setValue(0)
+}
+
+func (m *counterMetric) setValue(value int64) {
+	m.lock.Lock()
+	defer m.lock.Unlock()
+	m.value = value
 }
--- internal/metrics/gaugeMetric.go
+++ internal/metrics/gaugeMetric.go
@@ -1,16 +1,22 @@
 package metrics
 
-import "github.com/MaxReX92/go-yandex-aka-prometheus/internal/parser"
+import (
+	"sync"
+
+	"github.com/MaxReX92/go-yandex-aka-prometheus/internal/parser"
+)
 
 type gaugeMetric struct {
 	name  string
 	value float64
+	lock  sync.RWMutex
 }
 
 func NewGaugeMetric(name string) Metric {
 	return &gaugeMetric{
 		name:  name,
 		value: 0,
+		lock:  sync.RWMutex{},
 	}
 }
 
@@ -23,12 +29,20 @@ func (m *gaugeMetric) GetName() string {
 }
 
 func (m *gaugeMetric) GetStringValue() string {
+	m.lock.RLock()
+	defer m.lock.RUnlock()
 	return parser.FloatToString(m.value)
 }
 
 func (m *gaugeMetric) SetValue(value float64) {
-	m.value = value
+	m.setValue(value)
 }
 
 func (m *gaugeMetric) Flush() {
+}
+
+func (m *gaugeMetric) setValue(value float64) {
+	m.lock.Lock()
+	defer m.lock.Unlock()
+	m.value = value
 }
