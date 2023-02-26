1531c10fd8d7c4b6373b2934bc6cf0dfd00fe17d
Maxim Kuznetsov <d1sma@yandex.ru>
2023-02-26 21:21:54 +0300

    Fix config package

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -71,3 +71,23 @@ func createConfig() *config {
 		},
 	}
 }
+
+type config struct {
+	serverURL             string
+	pushTimeout           time.Duration
+	sendMetricsInterval   time.Duration
+	updateMetricsInterval time.Duration
+	collectMetricsList    []string
+}
+
+func (c *config) MetricsList() []string {
+	return c.collectMetricsList
+}
+
+func (c *config) MetricsServerURL() string {
+	return c.serverURL
+}
+
+func (c *config) PushMetricsTimeout() time.Duration {
+	return c.pushTimeout
+}
