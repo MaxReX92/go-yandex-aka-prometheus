68bb2ebd8dfc4b79ffc06698ea3b4bf9697afdfe
Maxim Kuznetsov <d1sma@yandex.ru>
2023-03-03 23:27:10 +0300

    Clear file storage

--- internal/storage/fileStorage.go
+++ internal/storage/fileStorage.go
@@ -1,7 +1,6 @@
 package storage
 
 import (
-	"bufio"
 	"encoding/json"
 	"fmt"
 	"github.com/MaxReX92/go-yandex-aka-prometheus/internal/logger"
@@ -15,6 +14,8 @@ type backupRecord struct {
 	Name  string `json:"name"`
 	Value string `json:"value"`
 }
+
+type backupRecords []*backupRecord
 
 type fileStorageConfig interface {
 	StoreFilePath() string
@@ -40,32 +41,21 @@ func (f *fileStorage) AddCounterMetricValue(name string, value int64) (int64, er
 }
 
 func (f *fileStorage) GetMetricValue(metricType string, metricName string) (float64, error) {
-	record, err := workWithFileResult[*backupRecord](f.lock, f.filePath, os.O_RDONLY|os.O_CREATE, func(fileStream *os.File) (*backupRecord, error) {
-		records, err := f.readRecords(fileStream, func(record *backupRecord) (bool, error) {
-			return record.Type == metricType && record.Name == metricName, nil
-		})
-		if err != nil {
-			return nil, err
-		}
-		if len(records) != 1 {
-			return nil, fmt.Errorf("metrics with name %v and type %v not found", metricName, metricType)
-		}
-
-		return records[0], nil
+	records, err := f.readRecordsFromFile(func(record *backupRecord) bool {
+		return record.Type == metricType && record.Name == metricName
 	})
-
 	if err != nil {
 		return 0, err
 	}
-	return parser.ToFloat64(record.Value)
+	if len(records) != 1 {
+		return 0, fmt.Errorf("metrics with name %v and type %v not found", metricName, metricType)
+	}
+
+	return parser.ToFloat64(records[0].Value)
 }
 
 func (f *fileStorage) GetMetricValues() (map[string]map[string]string, error) {
-	records, err := workWithFileResult[[]*backupRecord](f.lock, f.filePath, os.O_RDONLY|os.O_CREATE,
-		func(fileStream *os.File) ([]*backupRecord, error) {
-			return f.readRecords(fileStream, func(record *backupRecord) (bool, error) { return true, nil })
-		})
-
+	records, err := f.readRecordsFromFile(func(record *backupRecord) bool { return true })
 	if err != nil {
 		return nil, err
 	}
@@ -85,30 +75,26 @@ func (f *fileStorage) GetMetricValues() (map[string]map[string]string, error) {
 }
 
 func (f *fileStorage) Restore(metricValues map[string]map[string]string) error {
-	return workWithFile(f.lock, f.filePath, os.O_WRONLY|os.O_CREATE, func(fileStream *os.File) error {
-		records := []*backupRecord{}
-		for metricType, metricsByType := range metricValues {
-			for metricName, metricValue := range metricsByType {
-
-				records = append(records, &backupRecord{
-					Type:  metricType,
-					Name:  metricName,
-					Value: metricValue,
-				})
-			}
+	var records backupRecords
+	for metricType, metricsByType := range metricValues {
+		for metricName, metricValue := range metricsByType {
+			records = append(records, &backupRecord{
+				Type:  metricType,
+				Name:  metricName,
+				Value: metricValue,
+			})
 		}
+	}
 
-		return writeToFile(fileStream, records)
-	})
+	return f.writeRecordsToFile(records)
 }
 
 func (f *fileStorage) updateMetric(metricType string, metricName string, stringValue string) error {
-	return workWithFile(f.lock, f.filePath, os.O_RDWR|os.O_CREATE, func(fileStream *os.File) error {
-		records, err := f.readRecords(fileStream, func(record *backupRecord) (bool, error) {
-			// skip for append to end
-			return record.Type != metricType || record.Name != metricType, nil
+	// Read and write
+	return f.workWithFile(os.O_RDWR|os.O_CREATE, func(fileStream *os.File) error {
+		records, err := f.readRecords(fileStream, func(record *backupRecord) bool {
+			return record.Type != metricType || record.Name != metricType
 		})
-
 		if err != nil {
 			return err
 		}
@@ -118,67 +104,65 @@ func (f *fileStorage) updateMetric(metricType string, metricName string, stringV
 			Name:  metricName,
 			Value: stringValue,
 		})
-		return writeToFile(fileStream, records)
+		return f.writeRecords(fileStream, records)
 	})
 }
 
-func (f *fileStorage) readRecords(fileStream *os.File, isValid func(*backupRecord) (bool, error)) ([]*backupRecord, error) {
-	records := []*backupRecord{}
-	scanner := bufio.NewScanner(fileStream)
-
-	for scanner.Scan() {
-		record := &backupRecord{}
-		err := json.Unmarshal(scanner.Bytes(), record)
-		if err != nil {
-			logger.ErrorFormat("Unexpected string in backup file: %v", scanner.Text())
-			continue
-		}
-
-		valid, err := isValid(record)
-		if err != nil {
-			return nil, err
-		}
-		if !valid {
-			continue
-		}
-
-		records = append(records, record)
-	}
-
-	return records, scanner.Err()
+func (f *fileStorage) readRecordsFromFile(isValid func(*backupRecord) bool) (backupRecords, error) {
+	// ReadOnly
+	return f.workWithFileResult(os.O_RDONLY|os.O_CREATE, func(fileStream *os.File) (backupRecords, error) {
+		return f.readRecords(fileStream, isValid)
+	})
 }
 
-func writeToFile(fileStream *os.File, records []*backupRecord) error {
-	encoder := json.NewEncoder(fileStream)
+func (f *fileStorage) readRecords(fileStream *os.File, isValid func(*backupRecord) bool) (backupRecords, error) {
+	var records backupRecords
+	err := json.NewDecoder(fileStream).Decode(&records)
+	if err != nil {
+		logger.ErrorFormat("Fail to decode backup: %v", err)
+		return nil, err
+	}
+
+	result := backupRecords{}
 	for _, record := range records {
-		err := encoder.Encode(record)
-		if err != nil {
-			return err
+		if isValid(record) {
+			result = append(result, record)
 		}
 	}
 
-	return nil
+	return result, nil
 }
 
-func workWithFile(lock *sync.Mutex, filePath string, flag int, work func(file *os.File) error) error {
-	_, err := workWithFileResult(lock, filePath, flag, func(fileStream *os.File) (int, error) {
-		return 0, work(fileStream)
+func (f *fileStorage) writeRecordsToFile(records backupRecords) error {
+	// WriteOnly
+	return f.workWithFile(os.O_WRONLY|os.O_CREATE, func(fileStream *os.File) error {
+		return f.writeRecords(fileStream, records)
+	})
+}
+
+func (f *fileStorage) writeRecords(fileStream *os.File, records backupRecords) error {
+	return json.NewEncoder(fileStream).Encode(records)
+}
+
+func (f *fileStorage) workWithFile(flag int, work func(file *os.File) error) error {
+	_, err := f.workWithFileResult(flag, func(fileStream *os.File) (backupRecords, error) {
+		return nil, work(fileStream)
 	})
 	return err
 }
 
-func workWithFileResult[T any](lock *sync.Mutex, filePath string, flag int, work func(file *os.File) (T, error)) (T, error) {
-	var defaultResult T
-	if filePath == "" {
-		return defaultResult, nil
+func (f *fileStorage) workWithFileResult(flag int, work func(file *os.File) (backupRecords, error)) (backupRecords, error) {
+	if f.filePath == "" {
+		return nil, nil
 	}
 
-	lock.Lock()
-	defer lock.Unlock()
+	f.lock.Lock()
+	defer f.lock.Unlock()
 
-	fileStream, err := os.OpenFile(filePath, flag, 644)
+	fileStream, err := os.OpenFile(f.filePath, flag, 644)
 	if err != nil {
-		return defaultResult, err
+		logger.ErrorFormat("Fail to open file: %v", err.Error())
+		return nil, err
 	}
 	defer func(fileStream *os.File) {
 		err = fileStream.Close()
