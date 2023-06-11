a5e3f7dcac5995abf3de385e5db1763a6c4fdc3c
Maxim Kuznetsov <d1sma@yandex.ru>
2023-06-11 17:14:08 +0300

    Custom analysis implementation

--- internal/analysis/osExit.go
+++ internal/analysis/osExit.go
@@ -1,6 +1,10 @@
 package analysis
 
-import "golang.org/x/tools/go/analysis"
+import (
+	"go/ast"
+
+	"golang.org/x/tools/go/analysis"
+)
 
 var Analyzer = &analysis.Analyzer{
 	Name: "noosexit",
@@ -9,5 +13,30 @@ var Analyzer = &analysis.Analyzer{
 }
 
 func run(pass *analysis.Pass) (interface{}, error) {
+	for _, file := range pass.Files {
+		ast.Inspect(file, func(node ast.Node) bool {
+			switch x := node.(type) {
+			case *ast.File:
+				if x.Name.Name != "main" {
+					// not in main package
+					return false
+				}
+			case *ast.FuncDecl:
+				if x.Name.Name != "main" {
+					// not in main func
+					return false
+				}
+			case *ast.SelectorExpr:
+				if x.X != nil && x.Sel != nil {
+					ident, ok := x.X.(*ast.Ident)
+					if ok && ident.Name == "os" && x.Sel.Name == "Exit" {
+						pass.Reportf(ident.NamePos, "os.Exit called from main func")
+					}
+				}
+			}
+
+			return true
+		})
+	}
 	return nil, nil
 }
